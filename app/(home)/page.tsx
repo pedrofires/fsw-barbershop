675bda72e9f822d7fa8b4ef39fc522b4824fb53b
Pedro Fires <pedrof.fires@gmail.com>
2024-02-01 00:46:39 -0300

    feat: add welcome message

---
+++ app/(home)/page.tsx
@@ -0,0 +1,18 @@
+import { format } from "date-fns";
+import Header from "../_components/header";
+import { ptBR } from "date-fns/locale";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Pedro!</h2>
+        <p className="text-sm capitalize">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", { locale: ptBR })}
+        </p>
+      </div>
+    </div>
+  );
+}
