4cfa0d90266f664cf534de68c8e31e8df9192de1
Pedro Fires <pedrof.fires@gmail.com>
2024-02-03 02:22:37 -0300

    feat: added barbershops page and functionality to auth and side-menu call

---
+++ app/barbershops/[id]/_components/barbershop-info.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import SideMenu from "@/app/_components/side-menu";
+import { Button } from "@/app/_components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/app/_components/ui/sheet";
+import { Barbershop } from "@prisma/client";
+import { ChevronLeftIcon, MenuIcon } from "lucide-react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+interface BarbershopInfoProps {
+  barbershop: Barbershop;
+}
+
+const BarbershopInfo = ({ barbershop }: BarbershopInfoProps) => {
+  const router = useRouter();
+
+  const handleBackClick = () => {
+    router.replace("/");
+  };
+
+  return (
+    <div>
+      <div className="relative h-[250px] w-full">
+        <Button
+          onClick={handleBackClick}
+          size="icon"
+          variant="outline"
+          className="absolute left-4 top-4 z-50"
+        >
+          <ChevronLeftIcon />
+        </Button>
+
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button
+              size="icon"
+              variant="outline"
+              className="absolute right-4 top-4 z-50"
+            >
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+
+          <SheetContent className="p-0">
+            <SideMenu />
+          </SheetContent>
+        </Sheet>
+
+        <Image
+          src={barbershop.imageUrl}
+          fill
+          alt={barbershop.name}
+          style={{
+            objectFit: "cover",
+          }}
+          className="opacity-75"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default BarbershopInfo;
