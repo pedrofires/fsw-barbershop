4007c3fc0dfca11a9b703c4a4111c8baad21866e
Pedro Fires <pedrof.fires@gmail.com>
2024-02-03 03:32:18 -0300

    feat: add side menu

--- app/barbershops/[id]/_components/barbershop-info.tsx
+++ app/barbershops/[id]/_components/barbershop-info.tsx
@@ -4,7 +4,7 @@ import SideMenu from "@/app/_components/side-menu";
 import { Button } from "@/app/_components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/app/_components/ui/sheet";
 import { Barbershop } from "@prisma/client";
-import { ChevronLeftIcon, MenuIcon } from "lucide-react";
+import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
@@ -56,6 +56,18 @@ const BarbershopInfo = ({ barbershop }: BarbershopInfoProps) => {
           }}
           className="opacity-75"
         />
+      </div>
+
+      <div className="border-b border-solid border-secondary px-5 pb-6 pt-3">
+        <h1 className="text-xl font-bold">{barbershop.name}</h1>
+        <div className="mt-2 flex items-center gap-1">
+          <MapPinIcon className="text-primary" size={18} />
+          <p className="text-sm">{barbershop.address}</p>
+        </div>
+        <div className="mt-2 flex items-center gap-1">
+          <StarIcon className="text-primary" size={18} />
+          <p className="text-sm">5,0 (899 avaliações)</p>
+        </div>
       </div>
     </div>
   );
